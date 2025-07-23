b4feb260a45d176d9f6ecc32bec11ac80e8e7f65
Lyssenka <larissa.pf@proton.me>
2025-07-23 20:47:16 +0200

    Fix niri config

--- modules/home/gui/niri/config.nix
+++ modules/home/gui/niri/config.nix
@@ -178,9 +178,9 @@
         Mod+X { close-window; }
         "Mod+dead_circumflex" { spawn "kitty"; }
         Print { screenshot; }
-        XF86MonBrightnessDown { spawn "light" "-U" "5"; }
-        XF86MonBrightnessUp { spawn "light" "-A" "5"; }
-        XF86Display { spawn "obs" }
+        XF86KbdBrightnessDown { spawn "light" "-U" "5"; }
+        XF86KbdBrightnessUp { spawn "light" "-A" "5"; }
+        XF86Display { spawn "obs"; }
         XF86AudioLowerVolume { spawn "wpctl" "set-volume" "@DEFAULT_AUDIO_SINK@" "0.1-"; }
         XF86AudioMicMute { spawn "wpctl" "set-mute" "@DEFAULT_AUDIO_SOURCE@" "toggle"; }
         XF86AudioMute { spawn "wpctl" "set-mute" "@DEFAULT_AUDIO_SINK@" "toggle"; }

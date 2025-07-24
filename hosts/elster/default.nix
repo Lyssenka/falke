65eaf7396af306268f785c2f4a3b3431f62c34f9
Lyssenka <larissa.pf@proton.me>
2025-07-24 11:21:57 +0200

    Change gaming module to be opt-in

--- hosts/elster/default.nix
+++ hosts/elster/default.nix
@@ -1,4 +1,5 @@
-{config, ...}: {
+{ config, ... }:
+{
   imports = [
     # Include hardware scan and custom modules
     ./hardware-configuration.nix
@@ -7,14 +8,19 @@
 
   # We have to specify compression in the config for our Btrfs filesystem
   fileSystems = {
-    "/".options = ["compress=zstd"];
-    "/home".options = ["compress=zstd"];
-    "/nix".options = ["compress=zstd" "noatime"];
+    "/".options = [ "compress=zstd" ];
+    "/home".options = [ "compress=zstd" ];
+    "/nix".options = [
+      "compress=zstd"
+      "noatime"
+    ];
   };
   # Use Systemd-boot, change kernel to the latest one and prioritize loading the amd kernelmod
   boot = {
-    initrd.kernelModules = ["amdgpu"];
+    initrd.kernelModules = [ "amdgpu" ];
   };
+
+  vro.gui.gaming.enable = true;
 
   networking = {
     hostName = "elster";
--- modules/nixos/gui/gaming.nix
+++ modules/nixos/gui/gaming.nix
@@ -1,33 +1,46 @@
 {
+  config,
   pkgs,
   inputs,
+  lib,
   ...
-}: {
-  nix.settings = {
-    substituters = ["https://nix-gaming.cachix.org"];
-    trusted-public-keys = ["nix-gaming.cachix.org-1:nbjlureqMbRAxR1gJ/f3hxemL9svXaZF/Ees8vCUUs4="];
+}:
+let
+  inherit (lib) mkEnableOption mkIf;
+  cfg = config.vro.gui.gaming;
+in
+{
+  options.vro.gui.gaming = {
+    enable = mkEnableOption "Enable programs suited for gaming";
   };
 
-  programs.steam = {
-    enable = true;
-    gamescopeSession.enable = true;
-    remotePlay.openFirewall = true;
-    dedicatedServer.openFirewall = true;
-    extraCompatPackages = with pkgs; [proton-ge-bin];
-  };
+  config = mkIf cfg.enable {
+    nix.settings = {
+      substituters = [ "https://nix-gaming.cachix.org" ];
+      trusted-public-keys = [ "nix-gaming.cachix.org-1:nbjlureqMbRAxR1gJ/f3hxemL9svXaZF/Ees8vCUUs4=" ];
+    };
 
-  home-manager.sharedModules = [
-    {
-      home.packages = with pkgs; [
-        osu-lazer-bin
-        pcsx2
-        duckstation
-        itch
-        cemu
-        heroic
-        inputs.nix-gaming.packages.${pkgs.system}.mo2installer
-        inputs.nix-gaming.packages.${pkgs.system}.osu-stable
-      ];
-    }
-  ];
+    programs.steam = {
+      enable = true;
+      gamescopeSession.enable = true;
+      remotePlay.openFirewall = true;
+      dedicatedServer.openFirewall = true;
+      extraCompatPackages = with pkgs; [ proton-ge-bin ];
+    };
+
+    home-manager.sharedModules = [
+      {
+        home.packages = with pkgs; [
+          osu-lazer-bin
+          pcsx2
+          duckstation
+          itch
+          cemu
+          heroic
+          inputs.nix-gaming.packages.${pkgs.system}.mo2installer
+          inputs.nix-gaming.packages.${pkgs.system}.osu-stable
+        ];
+      }
+    ];
+  };
 }
